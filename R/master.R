f75dea5659b740cf29b9f22367531df28aa92b89
Ryszard Szymanski <ryszard.szymanski@outlook.com>
2018-08-28 09:57:19 +0200

    Add project source code

---
+++ R/master.R
@@ -0,0 +1,27 @@
+# Detect proper script_path (you cannot use args yet as they are build with tools in set_env.r)
+script_path <- (function() {
+  args <- commandArgs(trailingOnly = FALSE)
+  script_path <- dirname(sub("--file=", "", args[grep("--file=", args)]))
+  if (!length(script_path)) {
+    return("R")
+  }
+  if (grepl("darwin", R.version$os)) {
+    base <- gsub("~\\+~", " ", base) # on MacOS ~+~ in path denotes whitespace
+  }
+  return(normalizePath(script_path))
+})()
+
+# Setting .libPaths() to point to libs folder
+source(file.path(script_path, "set_env.R"), chdir = T)
+
+config <- load_config()
+args <- args_parser()
+
+library(drinkr)
+
+data_path <- file.path(script_path, "../data/drink_info.csv")
+
+drinkR(data_path = data_path,
+       port = args$get(name = "port", required = FALSE,  default = 7316),
+       host = args$get(name = "host", require = FALSE, default = "0.0.0.0"))
+
